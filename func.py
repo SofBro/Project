07a03a63b4046f74cfdcfb2593b4ebb84bd18b03
NicoleCu <32576333+NicoleCu@users.noreply.github.com>
2018-02-25 13:37:01 +0300

    Update func.py

--- func.py
+++ func.py
@@ -4,11 +4,17 @@
 
 a = []
 import xlrd
-f = xlrd.open_workbook("book123.xls")    #ты здесь зафигачь имя нужного файла
-s = f.sheet_by_index(0) #открывает нужный листик
-for row in range(s.nrows): #создает массив массивов
+
+f = xlrd.open_workbook("book123.xls")  # ты здесь зафигачь имя нужного файла
+s = f.sheet_by_index(0)  # открывает нужный листик
+for row in range(s.nrows):  # создает массив
     b = []
     for col in range(s.ncols):
         b.append(s.cell(row, col))
-    a.append((b))
+    for i in range(len(b)):
+        if i == 0:
+            e = str(b[i])
+        else:
+            e = e + str(b[i])
+    a.append(e)
 print(a)
